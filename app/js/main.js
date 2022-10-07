9612792186e18c0268281ec3b7d740ed2ba97ada
luciphoree <luciphoree@gmail.com>
2022-10-07 23:25:39 +0300

    update project

--- app/js/main.js
+++ app/js/main.js
@@ -41,25 +41,75 @@
 
 // }
 
-function points(games) {
-    let score = 0;
-    let result = [];
+// function points(games) {
+//     let score = 0;
+//     let result = [];
 
-    games.forEach(item => {
-        result = item.split(':')
-        if (result[0] > result[1]) {
-            score += 2
-        } else if (result[0] < result[1]) {
-            score += 0
-        } else if (result[0] == result[1]) {
-            score += 1
-        }
-    })
-    return score
+//     games.forEach(item => {
+//         result = item.split(':')
+//         if (result[0] > result[1]) {
+//             score += 2
+//         } else if (result[0] < result[1]) {
+//             score += 0
+//         } else if (result[0] == result[1]) {
+//             score += 1
+//         }
+//     })
+//     return score
 
-}
+// }
 
-let footballGame = ['1:15', '5:3', '2:2', '0:1']
+// let footballGame = ['1:15', '5:3', '2:2', '0:1']
 
 
-console.log(points(footballGame))
+// console.log(points(footballGame))
+
+// function scramble(st1) {
+//     let array = []
+//     let stLength = 0;
+//     for (i = 0; i < st1.length; i++) {
+//         let tempStr = ""
+//         for (j = 0; j < st1.length; j++) {
+
+//         }
+//     }
+//     stLength = st1.length;
+//     console.log(stLength)
+//     return array
+
+// }
+// let str = "kek"
+// console.log(scramble(str))
+
+// function even_or_odd(number) {
+//     let even = "Even"
+//     let odd = "Odd"
+//     if (number % 2 == 0) {
+//         return even
+//     } else {
+//         return odd
+//     }
+// }
+
+// console.log(even_or_odd(2))
+
+let str = "kek"
+let newStr = str.slice(1,-1)
+
+
+
+console.log(newStr)
+console.log(newStr[0])
+
+// function removeChar(str) {
+//     // if (!str) {
+//     //     return str
+//     // }
+//     let strLength = str.length
+//     for (i = strLength; i < str.length;i++){
+//         str.slice(i)
+//         console.log(str)
+//     }
+//   };
+
+//   console.log(removeChar(str))
